b2a55ce8b8178b39ef7fc378141186224b5064b8
JamesMorais <capic.2022118tads0048@aluno.ifpi.edu.br>
2023-11-23 16:09:35 -0300

    Atualizando Header e adicionando Sobre Nós

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ toggleBtn.onclick = (e) =>{
         disableDarkMode();
     }
 }
-
+/*
 document.addEventListener('DOMContentLoaded', function() {
     let sideBar = document.querySelector('.side-bar');
     let body = document.body;
@@ -73,7 +73,74 @@ document.addEventListener('DOMContentLoaded', function() {
             body.classList.remove('active');
         }
     };
+});*/
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    let sideBar = document.querySelector('.side-bar');
+    let body = document.body;
+
+    function updateSidebarVisibility() {
+        if (window.innerWidth >= 1200) {
+            sideBar.classList.add('active');
+            body.classList.add('active');
+        } else {
+            sideBar.classList.remove('active');
+            body.classList.remove('active');
+        }
+    }
+
+    updateSidebarVisibility();
+
+    window.addEventListener('resize', updateSidebarVisibility);
+
+    let profile = document.querySelector('.header .flex .profile');
+    let search = document.querySelector('.header .flex .search-form');
+    let menuBtn = document.querySelector('#menu-btn');
+    let closeBtn = document.querySelector('#close-btn');
+
+    menuBtn.onclick = () => {
+        sideBar.classList.toggle('active');
+        body.classList.toggle('active');
+    };
+
+    closeBtn.onclick = () => {
+        sideBar.classList.remove('active');
+        body.classList.remove('active');
+    };
+
+    window.onscroll = () => {
+        profile.classList.remove('active');
+        search.classList.remove('active');
+        if (window.innerWidth < 1200) {
+            sideBar.classList.remove('active');
+            body.classList.remove('active');
+        }
+    };
+
+    // Get the current page URL
+    let currentPage = window.location.href;
+
+    // Check if the current page is "sobreNos.html"
+    if (currentPage.includes("sobreNos.html")) {
+        // Remove the second div with id "user-btn"
+        let userBtn = document.getElementById('user-btn');
+        if (userBtn) {
+            userBtn.remove();
+        }
+    } else {
+        // Attach the event listener for userBtn if it exists
+        let userBtn = document.getElementById('user-btn');
+        if (userBtn) {
+            userBtn.onclick = () => {
+                profile.classList.toggle('active');
+                search.classList.remove('active');
+            };
+        }
+    }
 });
+
+
 
 
  
